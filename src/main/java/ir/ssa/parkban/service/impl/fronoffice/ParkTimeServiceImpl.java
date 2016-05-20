cce6316aef78b9c17c906aacc83dca506da6adfc
local-hym <yeganeh.hosein@gmail.com>
2016-05-20 13:41:29 +0430

    Filter

--- src/main/java/ir/ssa/parkban/service/impl/fronoffice/ParkTimeServiceImpl.java
+++ src/main/java/ir/ssa/parkban/service/impl/fronoffice/ParkTimeServiceImpl.java
@@ -3,7 +3,7 @@ package ir.ssa.parkban.service.impl.fronoffice;
 import ir.ssa.parkban.controller.dto.entity.ParkTimeDto;
 import ir.ssa.parkban.domain.entities.ParkTime;
 import ir.ssa.parkban.repository.ParkTimeDAO;
-import ir.ssa.parkban.vertical.util.ObjectMapper;
+import ir.ssa.parkban.vertical.core.util.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import ir.ssa.parkban.service.bean.frontoffice.ParkTimeService;
 import org.springframework.stereotype.Service;
