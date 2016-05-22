9814c66f6e870e80ce8af14766276544393f5470
local-hym <yeganeh.hosein@gmail.com>
2016-05-23 02:29:02 +0430

    Entity graph

---
+++ src/main/java/ir/ssa/parkban/vertical/core/domain/springcustom/CustomCrudMethodMetadata.java
@@ -0,0 +1,43 @@
+package ir.ssa.parkban.vertical.core.domain.springcustom;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.support.CrudMethodMetadata;
+
+import javax.persistence.LockModeType;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by hadoop on 5/22/16.
+ */
+public class CustomCrudMethodMetadata implements CrudMethodMetadata{
+
+    EntityGraph entityGraph;
+    Method method;
+
+    public CustomCrudMethodMetadata(EntityGraph entityGraph, Method method) {
+        this.entityGraph = entityGraph;
+        this.method = method;
+    }
+
+    @Override
+    public LockModeType getLockModeType() {
+        return LockModeType.NONE;
+    }
+
+    @Override
+    public Map<String, Object> getQueryHints() {
+        return new HashMap<>();
+    }
+
+    @Override
+    public EntityGraph getEntityGraph() {
+        return this.entityGraph;
+    }
+
+    @Override
+    public Method getMethod() {
+        return this.method;
+    }
+}
