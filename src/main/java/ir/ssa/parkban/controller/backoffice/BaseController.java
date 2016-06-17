e1c1144d788ddd9910a27de2a357ef3f58533562
local-hym <yeganeh.hosein@gmail.com>
2016-06-17 13:33:50 +0430

    Request body

--- src/main/java/ir/ssa/parkban/controller/backoffice/BaseController.java
+++ src/main/java/ir/ssa/parkban/controller/backoffice/BaseController.java
@@ -14,6 +14,7 @@ import ir.ssa.parkban.service.bean.frontoffice.ParkTimeService;
 import ir.ssa.parkban.vertical.core.domain.filterelement.StringFilter;
 import ir.ssa.parkban.vertical.core.domain.filterelement.StringFilterOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,7 +49,7 @@ public class BaseController {
     }
 
     @RequestMapping(value = "/insertRegion", method = RequestMethod.POST)
-    public RegionDto insertRegion(RegionDto region){
+    public RegionDto insertRegion(@RequestBody RegionDto region){
         return baseInformationService.insertRegion(region);
     }
 
