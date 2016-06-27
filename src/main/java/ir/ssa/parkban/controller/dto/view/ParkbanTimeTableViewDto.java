1dc65891f3c9469328a0af1af493a67f03472e63
local-hym <yeganeh.hosein@gmail.com>
2016-06-27 14:47:31 +0430

    park ban table dto

---
+++ src/main/java/ir/ssa/parkban/controller/dto/view/ParkbanTimeTableViewDto.java
@@ -0,0 +1,86 @@
+package ir.ssa.parkban.controller.dto.view;
+
+import ir.ssa.parkban.controller.dto.entity.ParkbanTimeTableDto;
+import ir.ssa.parkban.controller.dto.entity.RegionDto;
+import ir.ssa.parkban.domain.entities.ParkbanTimeTable;
+import ir.ssa.parkban.domain.entities.Region;
+
+/**
+ * Created by hadoop on 6/27/16.
+ */
+public class ParkbanTimeTableViewDto {
+
+    private RegionDto region;
+
+    private ParkbanTimeTableDto forSat;
+    private ParkbanTimeTableDto forSun;
+    private ParkbanTimeTableDto forMon;
+    private ParkbanTimeTableDto forTue;
+    private ParkbanTimeTableDto forWed;
+    private ParkbanTimeTableDto forThu;
+    private ParkbanTimeTableDto forFri;
+
+    public RegionDto getRegion() {
+        return region;
+    }
+
+    public void setRegion(RegionDto region) {
+        this.region = region;
+    }
+
+    public ParkbanTimeTableDto getForSat() {
+        return forSat;
+    }
+
+    public void setForSat(ParkbanTimeTableDto forSat) {
+        this.forSat = forSat;
+    }
+
+    public ParkbanTimeTableDto getForSun() {
+        return forSun;
+    }
+
+    public void setForSun(ParkbanTimeTableDto forSun) {
+        this.forSun = forSun;
+    }
+
+    public ParkbanTimeTableDto getForMon() {
+        return forMon;
+    }
+
+    public void setForMon(ParkbanTimeTableDto forMon) {
+        this.forMon = forMon;
+    }
+
+    public ParkbanTimeTableDto getForTue() {
+        return forTue;
+    }
+
+    public void setForTue(ParkbanTimeTableDto forTue) {
+        this.forTue = forTue;
+    }
+
+    public ParkbanTimeTableDto getForWed() {
+        return forWed;
+    }
+
+    public void setForWed(ParkbanTimeTableDto forWed) {
+        this.forWed = forWed;
+    }
+
+    public ParkbanTimeTableDto getForThu() {
+        return forThu;
+    }
+
+    public void setForThu(ParkbanTimeTableDto forThu) {
+        this.forThu = forThu;
+    }
+
+    public ParkbanTimeTableDto getForFri() {
+        return forFri;
+    }
+
+    public void setForFri(ParkbanTimeTableDto forFri) {
+        this.forFri = forFri;
+    }
+}
