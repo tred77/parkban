27c360bc3afb11daf2718f3bcdb06c53985a0b1f
local-hym <yeganeh.hosein@gmail.com>
2016-07-14 14:30:09 +0430

    oracle to h2

--- src/main/java/ir/ssa/parkban/vertical/configuration/JDBC/datasource/oracle/OracleDataSourceConfiguration.java
+++ src/main/java/ir/ssa/parkban/vertical/configuration/JDBC/datasource/oracle/OracleDataSourceConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OracleDataSourceConfiguration {
 
+
     /*@Bean
     DataSource dataSource() throws SQLException {
 
