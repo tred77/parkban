7b5d426a846edc351d000eb7ba26be1c9738055d
Behrouz-ZD <behruoz.zamani@gmail.com>
2016-08-22 00:33:08 +0430

    [enhancement] report

--- src/main/java/ir/ssa/parkban/service/impl/SettlementServiceImpl.java
+++ src/main/java/ir/ssa/parkban/service/impl/SettlementServiceImpl.java
@@ -1,10 +1,12 @@
 package ir.ssa.parkban.service.impl;
 
+import ir.ssa.parkban.domain.filters.ParkSettlementFilter;
 import ir.ssa.parkban.domain.filters.ParkUnSettlementFilter;
 import ir.ssa.parkban.domain.filters.RegionDailySettlementFilter;
 import ir.ssa.parkban.repository.ParkSettlementDAO;
 import ir.ssa.parkban.repository.ParkUnSettlementDAO;
 import ir.ssa.parkban.repository.RegionDailySettlementDAO;
+import ir.ssa.parkban.service.dto.entity.ParkSettlementDto;
 import ir.ssa.parkban.service.dto.entity.ParkTimeDto;
 import ir.ssa.parkban.domain.entities.ParkTime;
 import ir.ssa.parkban.domain.filters.ParkTimeFilter;
@@ -14,11 +16,16 @@ import ir.ssa.parkban.service.bean.SettlementService;
 import ir.ssa.parkban.service.dto.entity.ParkUnSettlementDto;
 import ir.ssa.parkban.service.dto.entity.RegionDailySettlementDto;
 import ir.ssa.parkban.vertical.core.domain.PagingList;
+import ir.ssa.parkban.vertical.core.domain.filterelement.DateFilter;
+import ir.ssa.parkban.vertical.core.domain.filterelement.DateFilterOperation;
+import ir.ssa.parkban.vertical.core.util.DateUtils.DateUtils;
 import ir.ssa.parkban.vertical.core.util.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -53,6 +60,13 @@ public class SettlementServiceImpl implements SettlementService {
         return ObjectMapper.map(parkTimeDAO.findAll(filter.getCriteriaExpression()),ParkTimeDto.class);
     }
 
+    @Override
+    public PagingList<ParkSettlementDto> findAllParkSettlement(ParkSettlementFilter filter) {
+        BaseService.setEntityGraph(parkSettlementDAO,filter,"findAll");
+        filter.setParkDate(DateUtils.convertToRangeDateIfEquals(filter.getParkDate()));
+        Page page = parkSettlementDAO.findAll(filter.getCriteriaExpression(),filter.getPageable());
+        return ObjectMapper.mapPagedList(page,ParkSettlementDto.class);
+    }
 
     @Override
     public void doParkSettlement() {
@@ -63,6 +77,7 @@ public class SettlementServiceImpl implements SettlementService {
     @Override
     public PagingList<ParkUnSettlementDto> findAllParkUnSettlement(ParkUnSettlementFilter filter) {
         BaseService.setEntityGraph(parkUnSettlementDAO,filter,"findAll");
+        filter.setParkDate(DateUtils.convertToRangeDateIfEquals(filter.getParkDate()));
         Page page = parkUnSettlementDAO.findAll(filter.getCriteriaExpression(),filter.getPageable());
         return ObjectMapper.mapPagedList(page,ParkUnSettlementDto.class);
     }
@@ -73,5 +88,7 @@ public class SettlementServiceImpl implements SettlementService {
         Page page = regionDailySettlementDAO.findAll(filter.getCriteriaExpression(),filter.getPageable());
         return ObjectMapper.mapPagedList(page,RegionDailySettlementDto.class);
     }
+
+
 
 }
